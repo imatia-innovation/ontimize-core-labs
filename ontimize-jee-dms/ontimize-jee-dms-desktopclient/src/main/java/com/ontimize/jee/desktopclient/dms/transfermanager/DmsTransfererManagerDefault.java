0117e34e27fa64fc98ef84c7f473ef45be80d857
Joaquin Romero <joaquin.romero@imatia.com>
2026-07-20 17:00:52 +0200

    [ontilabs-106683] BOM revision

--- ontimize-jee-dms/ontimize-jee-dms-desktopclient/src/main/java/com/ontimize/jee/desktopclient/dms/transfermanager/DmsTransfererManagerDefault.java
+++ ontimize-jee-dms/ontimize-jee-dms-desktopclient/src/main/java/com/ontimize/jee/desktopclient/dms/transfermanager/DmsTransfererManagerDefault.java
@@ -27,10 +27,6 @@ import com.ontimize.jee.common.tools.CheckingTools;
 import com.ontimize.jee.desktopclient.dms.transfermanager.AbstractDmsTransferable.Status;
 import com.ontimize.jee.desktopclient.dms.transfermanager.events.ITransferQueueListener;
 import com.ontimize.jee.desktopclient.dms.transfermanager.events.TransferQueueChangedEvent;
-import com.ontimize.jee.desktopclient.dms.upload.cloud.dropbox.DropboxDmsUploadable;
-import com.ontimize.jee.desktopclient.dms.upload.cloud.dropbox.DropboxDmsUploader;
-import com.ontimize.jee.desktopclient.dms.upload.cloud.gdrive.GoogleDriveDmsUploadable;
-import com.ontimize.jee.desktopclient.dms.upload.cloud.gdrive.GoogleDriveDmsUploader;
 import com.ontimize.jee.desktopclient.dms.upload.disk.LocalDiskDmsUploadable;
 import com.ontimize.jee.desktopclient.dms.upload.disk.LocalDiskDmsUploader;
 import com.ontimize.jee.desktopclient.dms.upload.web.WebDmsUploadable;
@@ -78,16 +74,14 @@ public class DmsTransfererManagerDefault implements Observer, IDmsTransfererMana
 		this.transferersFactory.put(LocalDiskDmsUploadable.class, new LocalDiskDmsUploader());
 		this.transferersFactory.put(WebDmsUploadable.class, new WebDmsUploader());
 		this.transferersFactory.put(DmsDownloadable.class, new DmsDownloader());
-		this.transferersFactory.put(GoogleDriveDmsUploadable.class, new GoogleDriveDmsUploader());
-		this.transferersFactory.put(DropboxDmsUploadable.class, new DropboxDmsUploader());
 	}
 
-	public void registerTransferer(Class<AbstractDmsTransferable> transferable, AbstractDmsTransferer<AbstractDmsTransferable> transferer) {
+	public void registerTransferer(final Class<AbstractDmsTransferable> transferable, final AbstractDmsTransferer<AbstractDmsTransferable> transferer) {
 		this.transferersFactory.put(transferable, transferer);
 	}
 
-	protected <T extends AbstractDmsTransferable> AbstractDmsTransferer<T> getTransfererForTransferable(Class<T> clazz) throws DmsException {
-		AbstractDmsTransferer<T> transferer = (AbstractDmsTransferer<T>) this.transferersFactory.get(clazz);
+	protected <T extends AbstractDmsTransferable> AbstractDmsTransferer<T> getTransfererForTransferable(final Class<T> clazz) throws DmsException {
+		final AbstractDmsTransferer<T> transferer = (AbstractDmsTransferer<T>) this.transferersFactory.get(clazz);
 		if (transferer == null) {
 			throw new DmsException("Transferer not found for " + clazz);
 		}
@@ -98,7 +92,7 @@ public class DmsTransfererManagerDefault implements Observer, IDmsTransfererMana
 		return this.syncFolder;
 	}
 
-	public void setSyncFolder(Path syncFolder) {
+	public void setSyncFolder(final Path syncFolder) {
 		this.syncFolder = syncFolder;
 	}
 
@@ -118,7 +112,7 @@ public class DmsTransfererManagerDefault implements Observer, IDmsTransfererMana
 	 * @throws DmsException
 	 */
 	@Override
-	public void transfer(AbstractDmsTransferable transferable) throws DmsException {
+	public void transfer(final AbstractDmsTransferable transferable) throws DmsException {
 		if (!transferable.getStatus().equals(Status.ON_PREPARE)) {
 			throw new DmsException("Transferable already initiated");
 		}
@@ -126,10 +120,10 @@ public class DmsTransfererManagerDefault implements Observer, IDmsTransfererMana
 	}
 
 	@Override
-	public void update(Observable observable, Object arg) {
+	public void update(final Observable observable, final Object arg) {
 		CheckingTools.failIf(!(observable instanceof AbstractDmsTransferable), "source is not AbstractDmsTransferable");
-		AbstractDmsTransferable transfer = (AbstractDmsTransferable) observable;
-		Status state = transfer.getStatus();
+		final AbstractDmsTransferable transfer = (AbstractDmsTransferable) observable;
+		final Status state = transfer.getStatus();
 		if (state.isFinishState()) {
 			this.fireTransferQueueChangedEvent(new TransferQueueChangedEvent(Collections.unmodifiableList(DmsTransfererManagerDefault.this.transfersList), transfer, null));
 			this.transfersList.remove(transfer);
@@ -137,24 +131,24 @@ public class DmsTransfererManagerDefault implements Observer, IDmsTransfererMana
 	}
 
 	@Override
-	public Path obtainDmsFileVersion(Serializable idFileVersion) throws DmsException {
-		EntityResult er = BeansFactory.getBean(IDMSService.class).fileVersionQuery(idFileVersion,
+	public Path obtainDmsFileVersion(final Serializable idFileVersion) throws DmsException {
+		final EntityResult er = BeansFactory.getBean(IDMSService.class).fileVersionQuery(idFileVersion,
 		        Arrays.asList(new String[] { DMSNaming.DOCUMENT_FILE_VERSION_FILE_SIZE, DMSNaming.DOCUMENT_FILE_NAME }));
-		Long fileSize = ((List<Number>) er.get(DMSNaming.DOCUMENT_FILE_VERSION_FILE_SIZE)).get(0).longValue();
-		String fileName = ((List<String>) er.get(DMSNaming.DOCUMENT_FILE_NAME)).get(0);
+		final Long fileSize = ((List<Number>) er.get(DMSNaming.DOCUMENT_FILE_VERSION_FILE_SIZE)).get(0).longValue();
+		final String fileName = ((List<String>) er.get(DMSNaming.DOCUMENT_FILE_NAME)).get(0);
 		return this.obtainDmsFileVersion(idFileVersion, fileName, fileSize);
 	}
 
 	@Override
-	public Path obtainDmsFileVersion(Serializable idFileVersion, String fileName, Long fileSize) throws DmsException {
-		Path localFile = this.computeLocalFile(idFileVersion, fileName);
+	public Path obtainDmsFileVersion(final Serializable idFileVersion, final String fileName, final Long fileSize) throws DmsException {
+		final Path localFile = this.computeLocalFile(idFileVersion, fileName);
 		if (!Files.exists(localFile)) {
 			final DmsDownloadable dmsDownloadable = new DmsDownloadable(idFileVersion, localFile, fileName, fileSize);
 			synchronized (dmsDownloadable) {
 				dmsDownloadable.addObserver(new Observer() {
 
 					@Override
-					public void update(Observable o, Object arg) {
+					public void update(final Observable o, final Object arg) {
 						if (dmsDownloadable.getStatus().isFinishState()) {
 							synchronized (dmsDownloadable) {
 								dmsDownloadable.notify();
@@ -165,7 +159,7 @@ public class DmsTransfererManagerDefault implements Observer, IDmsTransfererMana
 				this.transfer(dmsDownloadable);
 				try {
 					dmsDownloadable.wait();
-				} catch (InterruptedException error) {
+				} catch (final InterruptedException error) {
 					DmsTransfererManagerDefault.logger.error(null, error);
 				}
 			}
@@ -176,12 +170,12 @@ public class DmsTransfererManagerDefault implements Observer, IDmsTransfererMana
 		return localFile;
 	}
 
-	private Path computeLocalFile(Object idFileVersion, String filename) throws DmsException {
+	private Path computeLocalFile(final Object idFileVersion, final String filename) throws DmsException {
 		// TODO posiblemete haya un límete para el número máximo de ficheros por directorio
 		if (!Files.exists(this.syncFolder)) {
 			try {
 				Files.createDirectories(this.syncFolder);
-			} catch (IOException error) {
+			} catch (final IOException error) {
 				throw new DmsException(error.getMessage(), error);
 			}
 		}
@@ -189,17 +183,17 @@ public class DmsTransfererManagerDefault implements Observer, IDmsTransfererMana
 	}
 
 	@Override
-	public synchronized void addTransferQueueListener(ITransferQueueListener transferQueueListener) {
+	public synchronized void addTransferQueueListener(final ITransferQueueListener transferQueueListener) {
 		this.transferQueueListeners.add(transferQueueListener);
 	}
 
 	@Override
-	public synchronized void removeTransferQueueListener(ITransferQueueListener transferQueueListener) {
+	public synchronized void removeTransferQueueListener(final ITransferQueueListener transferQueueListener) {
 		this.transferQueueListeners.remove(transferQueueListener);
 	}
 
-	public synchronized void fireTransferQueueChangedEvent(TransferQueueChangedEvent evt) {
-		for (ITransferQueueListener listener : this.transferQueueListeners) {
+	public synchronized void fireTransferQueueChangedEvent(final TransferQueueChangedEvent evt) {
+		for (final ITransferQueueListener listener : this.transferQueueListeners) {
 			listener.onTransferQueueChanged(evt);
 		}
 	}
@@ -208,7 +202,7 @@ public class DmsTransfererManagerDefault implements Observer, IDmsTransfererMana
 
 		protected T transferable;
 
-		public TransferJob(T transferable) {
+		public TransferJob(final T transferable) {
 			super();
 			this.transferable = transferable;
 		}
@@ -216,13 +210,13 @@ public class DmsTransfererManagerDefault implements Observer, IDmsTransfererMana
 		@Override
 		public void run() {
 			try {
-				AbstractDmsTransferer<T> transferer = (AbstractDmsTransferer<T>) DmsTransfererManagerDefault.this.getTransfererForTransferable(this.transferable.getClass());
+				final AbstractDmsTransferer<T> transferer = (AbstractDmsTransferer<T>) DmsTransfererManagerDefault.this.getTransfererForTransferable(this.transferable.getClass());
 				DmsTransfererManagerDefault.this.transfersList.add(this.transferable);
 				this.transferable.addObserver(DmsTransfererManagerDefault.this);
 				DmsTransfererManagerDefault.this.fireTransferQueueChangedEvent(
 				        new TransferQueueChangedEvent(Collections.unmodifiableList(DmsTransfererManagerDefault.this.transfersList), null, this.transferable));
 				transferer.transfer(this.transferable);
-			} catch (Exception ex) {
+			} catch (final Exception ex) {
 				this.transferable.setStatus(Status.ERROR);
 				DmsTransfererManagerDefault.logger.error(null, ex);
 			}
