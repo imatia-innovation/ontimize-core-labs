11ce18a76249ff7d005b6f398934073f5e2d7ba7
Joaquin Romero <joaquin.romero@imatia.com>
2026-07-22 08:23:21 +0200

    [ontilabs-106683] BOM revision LOG systems

--- ontimize-jee-common/src/main/java/com/ontimize/jee/common/util/logging/log4j/Log4jManager.java
+++ ontimize-jee-common/src/main/java/com/ontimize/jee/common/util/logging/log4j/Log4jManager.java
@@ -1,15 +1,5 @@
 package com.ontimize.jee.common.util.logging.log4j;
 
-import com.ontimize.jee.common.util.logging.ILogManager;
-import com.ontimize.jee.common.util.logging.Level;
-import com.ontimize.jee.common.util.remote.BytesBlock;
-import org.apache.logging.log4j.core.Appender;
-import org.apache.logging.log4j.core.appender.RollingFileAppender;
-import org.apache.logging.slf4j.Log4jLogger;
-import org.slf4j.ILoggerFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -24,30 +14,40 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.appender.RollingFileAppender;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ontimize.jee.common.util.logging.ILogManager;
+import com.ontimize.jee.common.util.logging.Level;
+import com.ontimize.jee.common.util.remote.BytesBlock;
+
 
 public class Log4jManager implements ILogManager {
 
     private static final Logger logger = LoggerFactory.getLogger(Log4jManager.class);
 
     @Override
-    public Logger getLogger(String name) {
+    public Logger getLogger(final String name) {
         return LoggerFactory.getILoggerFactory().getLogger(name);
     }
 
     @Override
     public List<Logger> getLoggerList() {
-        ILoggerFactory loggerFactory = LoggerFactory.getILoggerFactory();
+        final ILoggerFactory loggerFactory = LoggerFactory.getILoggerFactory();
         if (!loggerFactory.getClass().isAssignableFrom(Log4jManager.getLog4jFactory())) {
             return null;
         }
 
-        List<org.slf4j.Logger> loggers = this.getLoggetList(loggerFactory);
-        List<org.slf4j.Logger> loggersComplete = this.completeLoggersHierarchly(loggers);
+        final List<org.slf4j.Logger> loggers = this.getLoggetList(loggerFactory);
+        final List<org.slf4j.Logger> loggersComplete = this.completeLoggersHierarchly(loggers);
         Collections.sort(loggersComplete, new LoggerNameComparetor());
         return loggersComplete;
     }
 
-    private List<Logger> completeLoggersHierarchly(List<Logger> loggers) {
+    private List<Logger> completeLoggersHierarchly(final List<Logger> loggers) {
         // TODO Consider to create "parent" unexisting package loggers
         return loggers;
     }
@@ -55,24 +55,24 @@ public class Log4jManager implements ILogManager {
     protected static Class getLog4jFactory() {
         try {
             return Class.forName("org.apache.logging.slf4j.Log4jLoggerFactory");
-        } catch (ClassNotFoundException e) {
+        } catch (final ClassNotFoundException e) {
             Log4jManager.logger.trace(null, e);
         }
         return null;
     }
 
     @Override
-    public Level getLevel(Logger logger) {
+    public Level getLevel(final Logger logger) {
         if (logger == null) {
             return null;
         }
 
         // Look again for logger by name UNDER PROPERLY CONTEXT -> The input logger is not correct, uses
         // "Default" context and has not the properly config
-        ILoggerFactory loggerFactory = LoggerFactory.getILoggerFactory();
-        Map loggersToUse = this.getValidLoggersToUse(loggerFactory);
-        org.apache.logging.log4j.core.Logger innerLogger = this.getInnerLogger(loggersToUse.get(logger.getName()));
-        org.apache.logging.log4j.Level cLevel = innerLogger.getLevel();
+        final ILoggerFactory loggerFactory = LoggerFactory.getILoggerFactory();
+        final Map loggersToUse = this.getValidLoggersToUse(loggerFactory);
+        final org.apache.logging.log4j.core.Logger innerLogger = this.getInnerLogger(loggersToUse.get(logger.getName()));
+        final org.apache.logging.log4j.Level cLevel = innerLogger.getLevel();
         if (cLevel == null) {
             return null;
         }
@@ -94,14 +94,14 @@ public class Log4jManager implements ILogManager {
     }
 
     @Override
-    public void setLevel(Logger logger, Level level) throws Exception {
+    public void setLevel(final Logger logger, final Level level) throws Exception {
         if (logger == null) {
             return;
         }
 
-        ILoggerFactory loggerFactory = LoggerFactory.getILoggerFactory();
-        Map loggersToUse = this.getValidLoggersToUse(loggerFactory);
-        org.apache.logging.log4j.core.Logger innerLogger = this.getInnerLogger(loggersToUse.get(logger.getName()));
+        final ILoggerFactory loggerFactory = LoggerFactory.getILoggerFactory();
+        final Map loggersToUse = this.getValidLoggersToUse(loggerFactory);
+        final org.apache.logging.log4j.core.Logger innerLogger = this.getInnerLogger(loggersToUse.get(logger.getName()));
 
         org.apache.logging.log4j.Level cLevel = null;
         switch (level) {
@@ -133,36 +133,36 @@ public class Log4jManager implements ILogManager {
     @Override
     public BytesBlock getFileLogger() {
         BytesBlock bb = null;
-        ILoggerFactory loggerFactory = LoggerFactory.getILoggerFactory();
+        final ILoggerFactory loggerFactory = LoggerFactory.getILoggerFactory();
         if (!loggerFactory.getClass().isAssignableFrom(Log4jManager.getLog4jFactory())) {
             return bb;
         }
         try {
-            Logger logger = loggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
-            Object innerLogger = getInnerLogger(logger);
+            final Logger logger = loggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+            final Object innerLogger = getInnerLogger(logger);
             if (innerLogger instanceof org.apache.logging.log4j.core.Logger) {
-                org.apache.logging.log4j.core.Logger logbackLogger = (org.apache.logging.log4j.core.Logger) innerLogger;
-                Map<String, Appender> appenderMap = logbackLogger.getAppenders();
-                Iterator<Appender> appenders = appenderMap.values().iterator();
+                final org.apache.logging.log4j.core.Logger logbackLogger = (org.apache.logging.log4j.core.Logger) innerLogger;
+                final Map<String, Appender> appenderMap = logbackLogger.getAppenders();
+                final Iterator<Appender> appenders = appenderMap.values().iterator();
                 while (appenders.hasNext()) {
-                    Appender appender = appenders.next();
+                    final Appender appender = appenders.next();
                     if (appender instanceof RollingFileAppender) {
-                        RollingFileAppender rp = (RollingFileAppender) appender;
-                        String data = this.readFile(new File(rp.getFileName()));
+                        final RollingFileAppender rp = (RollingFileAppender) appender;
+                        final String data = this.readFile(new File(rp.getFileName()));
                         bb = new BytesBlock(data.getBytes());
                     }
                 }
             }
-        } catch (Exception e) {
+        } catch (final Exception e) {
             Log4jManager.logger.error("Error retrieving data from log file.", e);
         }
         return bb;
     }
 
-    public String readFile(File f) {
-        ArrayDeque<String> queue = new ArrayDeque<String>(500);
+    public String readFile(final File f) {
+        final ArrayDeque<String> queue = new ArrayDeque<String>(500);
         this.readFromLast(queue, f, 500);
-        StringBuilder builder = new StringBuilder();
+        final StringBuilder builder = new StringBuilder();
         while (!queue.isEmpty()) {
             builder.append(queue.removeFirst());
             builder.append("\n");
@@ -170,7 +170,7 @@ public class Log4jManager implements ILogManager {
         return builder.toString();
     }
 
-    public void readFromLast(ArrayDeque<String> queue, File file, int lines) {
+    public void readFromLast(final ArrayDeque<String> queue, final File file, final int lines) {
         BufferedReader br = null;
         try {
             br = new BufferedReader(new FileReader(file));
@@ -180,22 +180,22 @@ public class Log4jManager implements ILogManager {
                 this.addToList(queue, line, lines);
                 line = br.readLine();
             }
-        } catch (FileNotFoundException e) {
+        } catch (final FileNotFoundException e) {
             Log4jManager.logger.error("File not found. ERROR: {}", e.getMessage(), e);
-        } catch (IOException e) {
+        } catch (final IOException e) {
             Log4jManager.logger.error("File can't be read. ERROR: {}", e.getMessage(), e);
         } finally {
             try {
                 if (br != null) {
                     br.close();
                 }
-            } catch (IOException e) {
+            } catch (final IOException e) {
                 Log4jManager.logger.error("Buffered reader can't be closed. ERROR:{}", e.getMessage(), e);
             }
         }
     }
 
-    protected void addToList(ArrayDeque<String> queue, String element, int lines) {
+    protected void addToList(final ArrayDeque<String> queue, final String element, final int lines) {
         if (queue.size() >= lines) {
             queue.removeFirst();
             queue.offer(element);
@@ -209,55 +209,55 @@ public class Log4jManager implements ILogManager {
     // return (org.apache.log4j.Logger) Log4jManager.getReflectionFieldValue(adapter, "logger");
     // }
 
-    private org.apache.logging.log4j.core.Logger getInnerLogger(Logger logger2) {
+    private org.apache.logging.log4j.core.Logger getInnerLogger(final Logger logger2) {
         return (org.apache.logging.log4j.core.Logger) Log4jManager.getReflectionFieldValue(logger2, "logger");
     }
 
-    private org.apache.logging.log4j.core.Logger getInnerLogger(Object logger2) {
+    private org.apache.logging.log4j.core.Logger getInnerLogger(final Object logger2) {
         return (org.apache.logging.log4j.core.Logger) Log4jManager.getReflectionFieldValue(logger2, "logger");
     }
 
     // For some extrange reason, when a lloger is requested to logerFactory it gets from a "Default"
     // context, and not from our own context.
-    private Map getValidLoggersToUse(ILoggerFactory loggerFactory) {
-        Map<Object, Map<String, Log4jLogger>> registry = (Map<Object, Map<String, Log4jLogger>>) Log4jManager
+    private Map getValidLoggersToUse(final ILoggerFactory loggerFactory) {
+		final Map<Object, Map<String, Object>> registry = (Map<Object, Map<String, Object>>) Log4jManager
             .getReflectionFieldValue(loggerFactory, "registry");
-        Map loggersToUse = registry.get(org.apache.logging.log4j.core.LoggerContext.getContext(false));
+        final Map loggersToUse = registry.get(org.apache.logging.log4j.core.LoggerContext.getContext(false));
         return loggersToUse;
     }
 
-    private List<org.slf4j.Logger> getLoggetList(ILoggerFactory loggerFactory) {
-        Map loggersToUse = this.getValidLoggersToUse(loggerFactory);
-        List<org.slf4j.Logger> list = new ArrayList<Logger>();
-        for (Object o : loggersToUse.values()) {
-            Logger logger2 = loggerFactory.getLogger((String) Log4jManager.getReflectionFieldValue(o, "name"));
+    private List<org.slf4j.Logger> getLoggetList(final ILoggerFactory loggerFactory) {
+        final Map loggersToUse = this.getValidLoggersToUse(loggerFactory);
+        final List<org.slf4j.Logger> list = new ArrayList<Logger>();
+        for (final Object o : loggersToUse.values()) {
+            final Logger logger2 = loggerFactory.getLogger((String) Log4jManager.getReflectionFieldValue(o, "name"));
             list.add(logger2);
         }
         return list;
     }
 
-    public static Object getReflectionFieldValue(Object toInvoke, String fieldName) {
+    public static Object getReflectionFieldValue(final Object toInvoke, final String fieldName) {
         try {
-            Field field = Log4jManager
+            final Field field = Log4jManager
                 .getReflectionField(toInvoke instanceof Class ? (Class<?>) toInvoke : toInvoke.getClass(), fieldName);
             field.setAccessible(true);
             return field.get(toInvoke instanceof Class ? null : toInvoke);
-        } catch (Exception error) {
+        } catch (final Exception error) {
             throw new RuntimeException(error);
         }
     }
 
-    public static Field getReflectionField(Class<?> cl, String fieldName) {
+    public static Field getReflectionField(final Class<?> cl, final String fieldName) {
         for (Class<?> innerClass = cl; innerClass != null; innerClass = innerClass.getSuperclass()) {
             try {
                 return innerClass.getDeclaredField(fieldName);
-            } catch (Exception error) {
+            } catch (final Exception error) {
                 Log4jManager.logger.trace(null, error);
                 // do nothing
-                for (Class<?> interfaceClass : innerClass.getInterfaces()) {
+                for (final Class<?> interfaceClass : innerClass.getInterfaces()) {
                     try {
                         return interfaceClass.getDeclaredField(fieldName);
-                    } catch (Exception err) {
+                    } catch (final Exception err) {
                         Log4jManager.logger.trace(null, err);
                     }
                 }
@@ -270,7 +270,7 @@ public class Log4jManager implements ILogManager {
     private final class LoggerNameComparetor implements Comparator<Logger> {
 
         @Override
-        public int compare(org.slf4j.Logger o1, org.slf4j.Logger o2) {
+        public int compare(final org.slf4j.Logger o1, final org.slf4j.Logger o2) {
             if ((o1 == null) || (o2 == null)) {
                 return 0;
             }
@@ -280,7 +280,7 @@ public class Log4jManager implements ILogManager {
     }
 
     @Override
-    public Object findAppenderOfType(Class interfaceOfAppender) {
+    public Object findAppenderOfType(final Class interfaceOfAppender) {
         return null;
     }
 
